654aacfb2b65b19abf92f3871fc702249f61debe
diegoski14 <diegosc1920@gmail.com>
2024-02-27 19:19:13 -0500

    TuProducto

--- app/src/main/java/com/example/myapplication/BeerExpert.java
+++ app/src/main/java/com/example/myapplication/BeerExpert.java
@@ -6,16 +6,23 @@ import java.util.List;
 public class BeerExpert {
 List<String> getMarcas(String color){
     List <String> marcas= new ArrayList<>();
-    if(color.equals("Red")){
-    marcas.add("Red Captain Beer");
-    marcas.add("RedSkull German Beer");
+    if(color.equals("Agricola")){
+    marcas.add("Bulto de semillas");
+    marcas.add("Abono ecologico");
+    marcas.add("Bulto de trigo");
+    marcas.add("Fertilizante");
     }else {
-        if (color.equals("Dark")) {
-            marcas.add("Darkside Moon Beer");
-            marcas.add("Darknight Beer");
+        if (color.equals("Liquido")) {
+            marcas.add("Agua");
+            marcas.add("Leche");
+            marcas.add("Vinagre");
+            marcas.add("Alcohol etilico");
         }else{
-            marcas.add("Anber Loot");
-            marcas.add("Beer bet");
+            marcas.add("Cemento");
+            marcas.add("Ceramica");
+            marcas.add("Aluminio");
+            marcas.add("Madera");
+            marcas.add("Plastico");
         }
     }
     return marcas;
